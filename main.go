de9e7a1d105df5ead379232bcead812c6e5322b7
HooEP01 <hooernping02@gmail.com>
2024-04-28 22:08:22 +0800

    add chatgpt api

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/sashabaranov/go-openai"
 )
 
 type RequestData struct {
@@ -18,9 +22,43 @@ type QuestionAnswer struct {
 	Answer   string `json:"answer"`
 }
 
+type ChatMessages struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type ChatResponse struct {
+	Id                string        `json:"id"`
+	Object            *string       `json:"object"`
+	Created           int           `json:"created"`
+	Model             string        `json:"model"`
+	SystemFingerprint *string       `json:"system_fingerprint"`
+	Choices           *[]ChatChoice `json:"choices"`
+	Usage             *ChatUsage    `json:"usage"`
+}
+
+type ChatUsage struct {
+	PromptTokens     *int `json:"prompt_tokens"`
+	CompletionTokens *int `json:"completion_tokens"`
+	TotalTokens      *int `json:"total_tokens"`
+}
+
+type ChatChoice struct {
+	Index        int           `json:"index"`
+	Message      *ChatMessages `json:"message"`
+	Logprobs     *string       `json:"logprobs"`
+	FinishReason *string       `json:"finish_reason"`
+}
+
 var loc, _ = time.LoadLocation("Asia/Kuala_Lumpur")
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("(E000) Failed to get env file.")
+	}
+
 	router := gin.Default()
 
 	router.POST("/message", func(c *gin.Context) {
@@ -108,7 +146,69 @@ func notifyAdminSupport(id string) {
 }
 
 func sendChatGPTRequest(question string) string {
-	return "This is chat gpt respond"
+	apiKey := os.Getenv("CHATGPT_API_KEY")
+	if apiKey != "" {
+		client := openai.NewClient(apiKey)
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model: openai.GPT3Dot5Turbo,
+				Messages: []openai.ChatCompletionMessage{
+					{
+						Role:    openai.ChatMessageRoleUser,
+						Content: question,
+					},
+				},
+			},
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		return resp.Choices[0].Message.Content
+
+		// data := map[string]interface{}{
+		// 	"model": "gpt-3.5-turbo",
+		// 	"messages": []ChatMessages{
+		// 		// {Role: "system", Content: "You are a helpful assistant."},
+		// 		{Role: "user", Content: question},
+		// 	},
+		// }
+
+		// body, err := json.Marshal(data)
+		// if err != nil {
+		// 	panic("(E001) Failed to json.Marshal(data).")
+		// }
+
+		// req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(body))
+		// if err != nil {
+		// 	panic("(E002) Failed to wrap http request.")
+		// }
+
+		// req.Header.Set("Authorization", "Bearer "+apiKey)
+		// req.Header.Set("Content-Type", "application/json")
+
+		// client := &http.Client{}
+		// resp, err := client.Do(req)
+		// if err != nil {
+		// 	panic("(E003) Failed to send request.")
+		// }
+
+		// defer resp.Body.Close()
+		// if resp.StatusCode > http.StatusBadRequest {
+		// 	panic("(E004) Status Code " + resp.Status)
+		// }
+
+		// var responseData ChatResponse
+		// if err := json.NewDecoder(resp.Body).Decode(&responseData); err != nil {
+		// 	panic("(E005) Failed to decode response data.")
+		// }
+
+		// return responseData.Choices[0].Message.Content
+	}
+
+	return "Api key not found"
 }
 
 func getQuestionAnswers(paramType string) []QuestionAnswer {
