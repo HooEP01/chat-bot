bedc81ab1c21b7ed1c529040349a579c18a1b8db
HooEP01 <hooernping02@gmail.com>
2024-04-28 18:05:31 +0800

    basic fuzzy

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.22.1
 
 require github.com/gin-gonic/gin v1.9.1
 
+require github.com/lithammer/fuzzysearch v1.1.8 // indirect
+
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
@@ -22,6 +24,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.1 // indirect
+	github.com/sahilm/fuzzy v0.1.1
+	github.com/sajari/fuzzy v1.0.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
 type RequestData struct {
 	Id       string `json:"id"`
 	Question string `json:"question"`
 }
+
+var loc, _ = time.LoadLocation("Asia/Kuala_Lumpur")
 
 func main() {
 	router := gin.Default()
@@ -23,11 +27,47 @@ func main() {
 		}
 
 		// Process data
+		respond, notification := respondToQuestion(data.Question)
+
+		// Notify admin support
+		if notification {
+			notifyAdminSupport(data.Id)
+		}
 
 		// Save to database
 
-		c.JSON(http.StatusCreated, data)
+		// Generate respondData
+		respondData := map[string]interface{}{
+			"id":         "uuid", // generate from database
+			"request_id": data.Id,
+			"answer":     respond,
+			"timestamp":  time.Now().UnixMilli(),
+		}
+
+		c.JSON(http.StatusCreated, respondData)
 	})
 
 	router.Run(":8080")
+}
+
+func respondToQuestion(question string) (string, bool) {
+	rules := map[string]string{
+		"hello":        "Hi there! How can I help you?",
+		"how are you?": "I'm just a bot, but thank for asking!",
+		"goodbye":      "Goodbye! if you have more questions, feel free to ask me!",
+	}
+
+	// Spelling and fuzzy check
+
+	for rule, response := range rules {
+		if fuzzy.Match(rule, question) {
+			return response, false
+		}
+	}
+
+	return "Please wait for a minutes, a customer service representation will help you for your question.", true
+}
+
+func notifyAdminSupport(id string) {
+
 }
