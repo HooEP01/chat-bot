48bc678d902edcca38cd66a99004998a10b8021b
HooEP01 <hooernping02@gmail.com>
2024-04-28 20:46:06 +0800

    faq and question type api

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ type RequestData struct {
 	Question string `json:"question"`
 }
 
+type QuestionAnswer struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
 var loc, _ = time.LoadLocation("Asia/Kuala_Lumpur")
 
 func main() {
@@ -27,11 +32,14 @@ func main() {
 		}
 
 		// Process data
-		respond, notification := respondToQuestion(data.Question)
+		respond, isNotifyNeeded := respondToQuestion(data.Question)
 
 		// Notify admin support
-		if notification {
+		if isNotifyNeeded {
 			notifyAdminSupport(data.Id)
+
+			// Or chatgpt api
+			respond = sendChatGPTRequest(data.Question)
 		}
 
 		// Save to database
@@ -42,6 +50,33 @@ func main() {
 			"request_id": data.Id,
 			"answer":     respond,
 			"timestamp":  time.Now().UnixMilli(),
+		}
+
+		c.JSON(http.StatusCreated, respondData)
+	})
+
+	router.GET("/message", func(c *gin.Context) {
+		paramType := c.Query("type")
+
+		// Or better get from database
+		questionAnswers := getQuestionAnswers(paramType)
+
+		// Generate respondData
+		respondData := map[string]interface{}{
+			"items":     questionAnswers,
+			"timestamp": time.Now().UnixMilli(),
+		}
+
+		c.JSON(http.StatusCreated, respondData)
+	})
+
+	router.GET("/type", func(c *gin.Context) {
+		messageType := [4]string{"sales", "partner", "support", ""}
+
+		// Generate respondData
+		respondData := map[string]interface{}{
+			"items":     messageType,
+			"timestamp": time.Now().UnixMilli(),
 		}
 
 		c.JSON(http.StatusCreated, respondData)
@@ -70,4 +105,36 @@ func respondToQuestion(question string) (string, bool) {
 
 func notifyAdminSupport(id string) {
 
+}
+
+func sendChatGPTRequest(question string) string {
+	return "This is chat gpt respond"
+}
+
+func getQuestionAnswers(paramType string) []QuestionAnswer {
+	questionAnswerList := map[string][]QuestionAnswer{
+		"sales": []QuestionAnswer{
+			{Question: "Plan A", Answer: "Plan A is ..."},
+			{Question: "Plan B", Answer: "Plan B is ..."},
+		},
+		"partner": []QuestionAnswer{
+			{Question: "Advertisement", Answer: "To advertise in here ..."},
+			{Question: "Integration", Answer: "Here is our criteria, please contact ..."},
+		},
+		"support": []QuestionAnswer{
+			{Question: "Mobile App", Answer: "To solve there problem, please go here ..."},
+			{Question: "Admin Portal", Answer: "To solve there problem, please go here ..."},
+		},
+		"": []QuestionAnswer{
+			{Question: "default", Answer: "This is default"},
+		},
+	}
+
+	questionAnswers, ok := questionAnswerList[paramType]
+
+	if !ok {
+		return []QuestionAnswer{}
+	}
+
+	return questionAnswers
 }
